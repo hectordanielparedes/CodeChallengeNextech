5df6118cfff2c4043648b598968a58c06aff27b4
Hector Daniel Paredes <hparedes@rocksolid.com>
2023-09-18 14:16:23 -0300

    adding validation in pipe

--- CodeChallengeNextech/ClientApp/src/app/home/filter.pipe.ts
+++ CodeChallengeNextech/ClientApp/src/app/home/filter.pipe.ts
@@ -10,7 +10,7 @@ export class FilterPipe implements PipeTransform {
     if ( search.length === 0 )
       return news.slice(page, page + itemsPerPage);
     
-    news = news.filter( item => item.title.toLocaleLowerCase().includes( search.toLocaleLowerCase() ) );
+    news = news.filter( item => item && item.title.toLocaleLowerCase().includes( search.toLocaleLowerCase() ) );
     return news.slice(page, page + itemsPerPage);
 
   }
